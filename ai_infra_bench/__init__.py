1489f6fd5585b8d9ba84af0445e6903ae86bc77b
Muqi Li <muqi1029@gmail.com>
2025-10-12 22:40:30 +0800

    add client_cmp in the __init__.py

--- ai_infra_bench/__init__.py
+++ ai_infra_bench/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-from ai_infra_bench.client import client_gen, client_slo
+from ai_infra_bench.client import client_cmp, client_gen, client_slo
 from ai_infra_bench.version import __version__
 
 logging.basicConfig(
@@ -9,4 +9,4 @@ logging.basicConfig(
     datefmt="%Y-%m-%d %H:%M:%S",
 )
 
-__all__ = ["__version__", "client_gen", "client_slo"]
+__all__ = ["__version__", "client_gen", "client_slo", "client_cmp"]
